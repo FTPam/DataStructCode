c72fb08227f417d07f4b517eb83a0dc1454b58dd
TPam <87828006+FTPam@users.noreply.github.com>
2021-11-15 13:14:49 +0800

    Update 递归：求二叉树中以元素值为x的结点为根的子树的深度.c

--- 6. 树/递归：求二叉树中以元素值为x的结点为根的子树的深度.c
+++ 6. 树/递归：求二叉树中以元素值为x的结点为根的子树的深度.c
@@ -1,13 +1,14 @@
 //求二叉树中以元素值为x的结点为根的子树的深度
 //bt为要查找的二叉树，x为目标位置的值， tag必须为0
 //depth为深度的起始值，必须为0
+//若二叉树中不存在值为x的结点，则会返回0
 int getDepthX(bitTree bt, elemType x, int tag, int depth) {
 	if (!bt) {
-		return depth - 1;	//如果传入的二叉树为空，则返回depth - 1，表示深度并未增加
-	}
+		return depth - 1;
+	}	//如果传入的二叉树为空，则返回depth - 1，表示深度并未增加
 	if (bt->data == x && !tag) {
 		return getDepthX(bt, x, 1, 0) + 1;
-	}	//刚好找到根节点，则将tag改为1然后继续递归调用函数
+	}	//此时将递归函数从“寻找模式”切换到“求深度模式”
 	if (!tag) {
 		int LNum = 0, RNum = 0;
 		if (bt->LChild)
@@ -15,7 +16,8 @@ int getDepthX(bitTree bt, elemType x, int tag, int depth) {
 		if (bt->RChild)
 			RNum = getDepthX(bt->RChild, x, 0, 0);
 		return LNum ? LNum : RNum;
-	}	//若未找到，且tag还为0，则先继续找x
+	}	//若未找到，则继续在“寻找模式”下寻找根节点
+	//这部分代码是递归求深度
 	int LDepth, RDepth;
 	LDepth = getDepthX(bt->LChild, x, tag, depth + 1);
 	RDepth = getDepthX(bt->RChild, x, tag, depth + 1);
