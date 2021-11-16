7a976d27ab3dfd6baddbab1a4171b06cdfe1a6e4
TPam <87828006+FTPam@users.noreply.github.com>
2021-11-16 20:50:35 +0800

    Create 将顺序存储的二叉树存入二叉链表.c
    假设以顺序表sa表示一棵完全二叉树，sa.elem[sa.last]中存放树中每个结点的数据元素。试编写算法由此顺序存储结构建立该二叉树的二叉链表。

---
+++ 6. 树/将顺序存储的二叉树存入二叉链表.c
@@ -0,0 +1,17 @@
+//将顺序存储的二叉树存入二叉链表中存储
+bitTree ConvertTree(sqBitTree bt, int num) {
+	if (bt.length < 0 || num < 1 || num > bt.length) {
+		return NULL;
+	}//检查数据合法性
+	//创建结点
+	bitNode* node = createNode();
+	node->data = bt.data[num - 1];
+	//递归为其接上左右孩子
+	if (2 * num <= bt.length) {
+		node->LChild = ConvertTree(bt, 2 * num);
+	}
+	if (2 * num + 1 <= bt.length) {
+		node->RChild = ConvertTree(bt, 2 * num + 1);
+	}
+	return node;
+}
