a80d83173e03ad5d8fd62a8a61a261d72f0a4246
TPam <87828006+FTPam@users.noreply.github.com>
2021-12-11 01:51:19 +0800

    Update 建立AOE储存网络，并输出ve[]和vl[].cpp

--- 8. 图/建立AOE储存网络，并输出ve[]和vl[].cpp
+++ 8. 图/建立AOE储存网络，并输出ve[]和vl[].cpp
@@ -1,16 +1,18 @@
 #pragma warning(disable:4996)
+#include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <queue>
 using namespace std;
 
 #define MAX_VERTEX_NUM 26
+
 // 邻接表结构体
 typedef char elemType;	//图所储存的数据类型
 // 邻接表结点
 typedef struct ArcNode {
-	int adjvex;	//在顶点表中的下标
-	double weight;	//该边的权重
+	int adjvex;	//指向的顶点在顶点表中的下标
+	int weight;	//该边的权重
 	ArcNode* nextArc;	//下一个邻接边
 }ArcNode;
 // 顶点表
@@ -19,32 +21,19 @@ typedef struct VNode {
 	bool isTed;	//是否被访问过
 	ArcNode* firstArc;
 }VNode, AdjList[MAX_VERTEX_NUM];
-// 图结构体
-typedef struct Graph { 
+// AOE网络结构体
+typedef struct Graph {
 	AdjList vex;	//顶点表
 	int vexNum, arcNum;	//顶点数量、边数量
 }Graph;
-// 在完成一次遍历后，检查是否有未连通的结点并输出
-void checkSolo(Graph& g) {
-	cout << "未连通结点为: ";
-	bool tag = false;
-	for (int i = 0; i < g.vexNum; i++) {
-		if (!g.vex[i].isTed) {
-			cout << g.vex[i].data << ' ';
-			tag = true;
-		}
-	}
-	if (!tag) {
-		cout << "无";
-	}
-}
 // 给图添加边
 // g为要添加的图，vex为顶点在顶点表中的下标
 // des为相邻顶点在顶点表中的下标
-void addEdge(Graph& g, int vex, int des, double weight) {
+void addEdge(Graph& g, int vex, int des, int weight) {
 	ArcNode* p = g.vex[vex].firstArc;
 	ArcNode* node = (ArcNode*)malloc(sizeof(ArcNode));
 	node->adjvex = des;
+	node->weight = weight;
 	node->nextArc = NULL;
 	// 如果还没有第一条边，则加上
 	if (!p) {
@@ -52,73 +41,68 @@ void addEdge(Graph& g, int vex, int des, double weight) {
 	}
 	else {
 		// 移动到最后一个结点
-		while (p->nextArc!=NULL) {
+		while (p->nextArc != NULL) {
 			p = p->nextArc;
 		}
 		p->nextArc = node;
 	}
 }
-// 深度优先遍历，仅遍历连通结点
-// g为要遍历的图，v为顶点序号
-// 调用时，v一般应该传入0
-void dfs(Graph& g, int v) {
-	//如果没有结点，直接返回
-	if (g.vexNum == 0) {
-		return;
-	}
-	//输出当前结点
-	cout << g.vex[v].data << ' ';
-	g.vex[v].isTed = true;
-	//递归深入
-	ArcNode* p = g.vex[v].firstArc;
-	while (p != NULL) {
-		if (!g.vex[p->adjvex].isTed) {
-			dfs(g, p->adjvex);
-		}
-		p = p->nextArc;
-	}
-}
-// 广度优先遍历，仅遍历连通结点
-// g为要遍历的图
-void bfs(Graph& g) {
+// 获取Ve和Vi数组
+void bfs(Graph& g, int VE[], int VL[]) {
 	//如果没有结点，直接返回
 	if (g.vexNum == 0) {
 		return;
 	}
 	//创建辅助队列并让第一个结点入队列
 	queue <int> q;
+	int re[MAX_VERTEX_NUM], i = 0;
 	q.push(0);
 	g.vex[0].isTed = true;
+	//计算VE
+	int last;	//记录网络终点
 	while (!q.empty()) {
-		//输出当前结点并标记为已访问过
-		cout << g.vex[q.front()].data << ' ';
-		//将与该节点相邻的所有结点入队列
+		//遍历整个邻接表
 		ArcNode* p = g.vex[q.front()].firstArc;
+		re[i++] = q.front();
 		while (p != NULL) {
+			//将还未访问过的进行处理
 			if (!g.vex[p->adjvex].isTed) {
 				q.push(p->adjvex);
+				VE[p->adjvex] = VE[q.front()] + p->weight;
 				g.vex[p->adjvex].isTed = true;
 			}
+			else {
+				//如果当前路径获得的权重大于原权重，则更新
+				if (VE[q.front()] + p->weight > VE[p->adjvex]) {
+					VE[p->adjvex] = VE[q.front()] + p->weight;
+				}
+			}
+			VL[p->adjvex] = VE[p->adjvex];	//初始化一遍VL
 			p = p->nextArc;
 		}
 		//删除当前结点
+		last = q.front();
 		q.pop();
 	}
-}
-// 重新初始化图，将所有结点都设置为未访问过
-void initalG(Graph& g) {
-	for (int i = 0; i < g.vexNum; i++) {
-		g.vex[i].isTed = false;
+	i = i - 2;
+	while (i > 0) {
+		//遍历整个邻接表
+		ArcNode* p = g.vex[i].firstArc;
+		while (p != NULL) {
+			if (VL[p->adjvex] - p->weight > VL[i]) {
+				VL[i] = VL[p->adjvex] - p->weight;
+			}
+			p = p->nextArc;
+		}
+		i--;
 	}
 }
-// 主函数
-int main() {
-	char fileName[] = "source.txt";
-	cout << "正在从" << fileName << "读入数据..." << endl;
+// 从文件读入数据
+// 格式：顶点为第一行，以空格分割
+// 邻接表以 下一节点编号-权重, 的格式输入，每一行以分号结束
+void readFile(Graph& g, char fileName[]) {
 	ifstream  file;
 	file.open(fileName, ios::in);
-	
-	Graph g;
 	int vNum;
 	// 读入结点
 	for (vNum = 0; file.peek() != '\n'; vNum++) {
@@ -127,30 +111,47 @@ int main() {
 		g.vex[vNum].firstArc = NULL;
 	}
 	g.vexNum = vNum;
+	g.arcNum = 0;
 	// 读入边
 	file.ignore(1);	//清除掉上一行的换行符
-	for (int i = 0; !file.eof() && i < g.vexNum; i++) {
+	for (int i = 0; i < g.vexNum - 1; i++) {
 		while (1) {
-			if (file.peek() == '\n' || file.eof()) {
-				file.ignore(1);
-				break;	//换行
+			char temp = file.peek();
+			if (file.peek() == ';' || file.eof()) {
+				file.ignore(1);	//跳过分号
+				break;	//开始记录邻接表的下一行
 			}
-			else {
-				int adj;
-				double weight;
-				file >> adj;
-				file >> weight;
-				addEdge(g, i, adj, weight);
+			else if (file.peek() == ',') {
+				file.ignore(1);	//跳过逗号
+				char temp = file.peek();
 			}
+			int adj;
+			int weight;
+			file >> adj >> weight;
+			addEdge(g, i, adj, -weight);
+			g.arcNum++;
 		}
 	}
+
+}
+// 主函数
+int main() {
+	char fileName[] = "source.txt";
+	Graph g;
+	cout << "正在从" << fileName << "读入数据..." << endl;
+	readFile(g, fileName);
 	cout << "数据读入完毕..." << endl << endl;
-	cout << "深度优先遍历结果: ";
-	dfs(g, 0);
-	checkSolo(g);
-	initalG(g);
-	cout << endl << "广度优先遍历结果: ";
-	bfs(g);
-	checkSolo(g);
+
+	int VE[MAX_VERTEX_NUM] = { 0 }, VL[MAX_VERTEX_NUM] = { 0 };
+	bfs(g, VE, VL);
+
+	cout << "VE[]: ";
+	for (int i = 0; i < g.vexNum; i++) {
+		printf("%3d", VE[i]);
+	}
+	cout << endl << "VL[]: ";
+	for (int i = 0; i < g.vexNum; i++) {
+		printf("%3d", VL[i]);
+	}
 	cout << endl;
 }
